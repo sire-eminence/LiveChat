fdff1d4b8f42ae9b490c655887750c2656d44aac
sire-eminence <122898945+sire-eminence@users.noreply.github.com>
2023-02-10 00:16:14 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
+"use strict";
 
+//Logout script
+let out = document.querySelector('.out');
+out.addEventListener('click', ()=>{
+    let ans = confirm("Are you sure you want to exit chat?");
+    if(ans == true){
+        window.location.assign("http://localhost/livechat/logout.php")
+    }
+});
+
+
+//Submit message form
+let chatForm = document.querySelector('.msgForm'),
+url = "http://localhost/livechat/post.php";
+
+if(chatForm){
+	chatForm.addEventListener('submit', function(e){
+		let formdata = new FormData(chatForm);
+        
+        fetch(url, {
+            method: "POST",
+            body: formdata,
+        });
+
+        chatForm.reset();
+
+		e.preventDefault();
+	});
+}
+
+
+//Loading new messages
+let msgPane = document.querySelector('.msgPane'),
+myHeaders = new Headers();
+
+myHeaders.append('pragma', 'no-cache');
+myHeaders.append('cache-control', 'no-cache');
+
+const loadMsgLog = ()=>{
+    fetch("http://localhost/livechat/log.txt", {
+		method: "GET",
+        headers: myHeaders,
+	})
+    .then((response) => {
+		if(response.ok){
+			return response.text();
+		}
+	})
+    .then((result) => {
+        msgPane.textContent = "";
+        msgPane.insertAdjacentHTML('beforeend', result);
+    });
+
+    msgPane.scrollTop = "1000";
+}
+
+//Call the load messages function within 3 minutes interval
+setInterval(loadMsgLog, 3000);
